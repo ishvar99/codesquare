1ffcf9c16453104b4c893d61efdc7c9e63c50adb
ishvar99 <ishanvarshney99@gmail.com>
2021-04-06 10:38:31 +0530

    optimizing the plugin to accomodate any npm package

--- src/plugins/unplug-path-plugin.ts
+++ src/plugins/unplug-path-plugin.ts
@@ -1,12 +1,18 @@
 import * as esbuild from 'esbuild-wasm';
-
+import axios from 'axios';
 export const unpkgPathPlugin = () => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
+     // it hijacks esbuild default behaviour for looking a package in local file system
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log('onResole', args);
+        console.log('onResolve', args);
+        if(args.path==='index.js'){
         return { path: args.path, namespace: 'a' };
+        }
+        
+        return {path:`https://unpkg.com/${args.path}`,namespace:'a'}
+        
       });
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
@@ -16,16 +22,25 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-              import message from './message';
+              import message from 'tiny-test-pkg';    
               console.log(message);
             `,
           };
-        } else {
-          return {
-            loader: 'jsx',
-            contents: 'export default "hi there!"',
-          };
-        }
+        } 
+        const {data}= await axios.get('https://unpkg.com/tiny-test-pkg@1.0.0/index.js');
+        return {
+         loader: 'jsx',
+         contents: `
+           import message from 'tiny-test-pkg';    
+           console.log(message);
+         `,
+       };
+        // else {
+        //   return {
+        //     loader: 'jsx',
+        //     contents: 'export default "hi there!"',
+        //   };
+        // }
       });
     },
   };
