5bb10eca229f2f32e9f891a8bf91621e81085369
ishvar99 <ishanvarshney99@gmail.com>
2021-04-06 15:31:00 +0530

    fixed single quotes error

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -21,9 +21,21 @@ const fileCache= localforage.createInstance({
    if(cachedResult)
    return cachedResult;
    const {data,request}= await axios.get(args.path);
+   const fileType = args.path.match(/.css$/)? 'css':'jsx'
+   const escaped = data
+          .replace(/\n/g, '')
+          .replace(/"/g, '\\"')
+          .replace(/'/g, "\\'");
+   const contents = 
+   fileType==='css'?
+   `const style =document.createElement('style');
+   style.innerText='${escaped}'; 
+   document.head.appendChild(style);`
+   :data;
    const result:esbuild.OnLoadResult = {
-     loader: 'jsx',
-     contents: data,
+     loader: 'jsx',  
+     //loader: css ,will not work as it needs to store the outfile on the local device
+     contents,
      resolveDir: new URL('./',request.responseURL).pathname
    }
    await fileCache.setItem(args.path,result);
