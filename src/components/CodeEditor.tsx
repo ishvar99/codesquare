6b7c388bc3c6dbfe6631ffbeecfd7dfd7d4637fa
ishvar99 <ishanvarshney99@gmail.com>
2021-04-08 18:22:13 +0530

    added more configuration options

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -4,6 +4,13 @@ const CodeEditor = () => {
  return(
   <Editor theme="vs-dark" options={{
    wordWrap:'on',
+   minimap:{enabled:false},
+   showUnused:false,
+   folding:false,
+   lineNumbersMinChars:3,
+   fontSize:16,
+   scrollBeyondLastLine:false,
+   automaticLayout:true,
   }} language="javascript" height="500px"/>
  )
 }
