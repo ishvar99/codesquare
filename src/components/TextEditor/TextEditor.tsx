028d87c3bf9175d0845dd3388a3262ba81059b76
ishvar99 <ishanvarshney99@gmail.com>
2021-04-10 15:51:58 +0530

    resolving a edge case

--- src/components/TextEditor/TextEditor.tsx
+++ src/components/TextEditor/TextEditor.tsx
@@ -1,19 +1,23 @@
-import React,{useState,useEffect} from 'react'
+import React,{useState,useEffect,useRef} from 'react'
 import MDEditor from '@uiw/react-md-editor';
 const TextEditor = () => {
+ const ref= useRef<HTMLDivElement|null>(null);
  useEffect(() => {
-  const listener = ()=>{
+  const listener = (event:MouseEvent)=>{
+   if(ref.current && event.target && ref.current.contains(event.target as Node)){
+    return;
+   }
    setEditing(false);
   }
   window.addEventListener('click',listener,{capture:true})
   return () => {
-   //event bubbling
+   //event capturing
    window.removeEventListener('click',listener,{capture:true})
   }
  }, [])
  const [editing, setEditing] = useState(false)
  if(editing){
-  return  <div>
+  return  <div ref={ref}>
   <MDEditor/>
  </div>
  }
