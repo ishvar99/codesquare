3d525448c374425a744dd3b4086c5cabd272decc
ishvar99 <ishanvarshney99@gmail.com>
2021-04-10 18:40:45 +0530

    implemented MOVE_CELL reducer

--- src/redux/reducers/cellsReducers.ts
+++ src/redux/reducers/cellsReducers.ts
@@ -20,13 +20,24 @@ const intialState: CellsState={
 const reducer  = produce((state:CellsState=intialState,action:Action):CellsState|void=>{
  switch(action.type){
   case Types.MOVE_CELL:{
-   return state;
+   const {id, direction}=action.payload;
+   const index  = state.order.findIndex((i)=>i===id)
+   const targetIndex =direction==='up'?index-1:index+1;
+   if(targetIndex<0 || targetIndex>state.order.length-1){
+   return;
+   }
+   state.order[index]=state.order[targetIndex]
+   state.order[targetIndex]=action.payload.id
+   return;
   }
   case Types.INSERT_CELL_BEFORE:{
    return state;
   }
   case Types.DELETE_CELL:{
-   return state;
+   const id=action.payload;
+   delete state.data[id];
+   state.order=state.order.filter((e)=>e!==id); // remove cell from order array
+   return;
   }
   case Types.UPDATE_CELL:{
    const {id,content}=action.payload
