af33de9ff7841fd9ed137158e71324bc76dcd2d8
ishvar99 <ishanvarshney99@gmail.com>
2021-04-10 16:11:35 +0530

    adding value state to the markdown editor

--- src/components/TextEditor/TextEditor.tsx
+++ src/components/TextEditor/TextEditor.tsx
@@ -3,6 +3,7 @@ import './TextEditor.css'
 import MDEditor from '@uiw/react-md-editor';
 const TextEditor = () => {
  const ref= useRef<HTMLDivElement|null>(null);
+ const [value, setValue] = useState('# Header')
  useEffect(() => {
   const listener = (event:MouseEvent)=>{
    if(ref.current && event.target && ref.current.contains(event.target as Node)){
@@ -19,11 +20,11 @@ const TextEditor = () => {
  const [editing, setEditing] = useState(false)
  if(editing){
   return  <div className='text-editor' ref={ref}>
-  <MDEditor/>
+  <MDEditor onChange={(v)=>setValue(v||'')} value={value}/>
  </div>
  }
  return (
- <div className='text-editor' onClick={()=>setEditing(true)}><MDEditor.Markdown source={'# Header'}/></div>
+ <div className='text-editor' onClick={()=>setEditing(true)}><MDEditor.Markdown source={value}/></div>
  )
 }
 
