9f97dd141c81046424ef15d6c21c0dc084122278
ishvar99 <ishanvarshney99@gmail.com>
2021-04-12 17:51:16 +0530

    styled action bar

--- src/components/ActionBar/ActionBar.tsx
+++ src/components/ActionBar/ActionBar.tsx
@@ -7,9 +7,15 @@ const ActionBar:React.FC<ActionBarProps> = ({id}) => {
  const {moveCell,deleteCell} =useActions();
  return (
   <div>
-   <button onClick={(e)=>moveCell(id,'up')}>Up</button>
-   <button onClick={(e)=>moveCell(id,'down')}>Down</button>
-   <button onClick={(e)=>deleteCell(id)}>Delete</button>
+   <button className="button is-primary is-small" onClick={(e)=>moveCell(id,'up')}><span className='icon'>
+    <i className="fas fa-arrow-up"></i>
+    </span></button>
+   <button className="button is-primary is-small" onClick={(e)=>moveCell(id,'down')}><span className='icon'>
+    <i className="fas fa-arrow-down"></i>
+    </span></button>
+   <button className="button is-primary is-small" onClick={(e)=>deleteCell(id)}><span className='icon'>
+    <i className="fas fa-times"></i>
+    </span></button>
   </div>
  )
 }
