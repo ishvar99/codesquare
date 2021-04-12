52876e25379de3d5d3f7ca1ebec45f92e735133d
ishvar99 <ishanvarshney99@gmail.com>
2021-04-12 17:02:36 +0530

    passing redux state to CodeCell Component as props

--- src/components/CellListItem/CellListItem.tsx
+++ src/components/CellListItem/CellListItem.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import {Cell} from '../../redux/cell'
+import CodeCell from '../CodeCell/CodeCell'
+import TextEditor from '../TextEditor/TextEditor'
 interface CellListItemProps{
  cell:Cell
 }
 const CellListItem: React.FC<CellListItemProps> = ({cell}) => {
  return (
   <div>
-   {cell.id}
+   {cell.type==='code'?<CodeCell cell={cell}/>:<TextEditor/>}
   </div>
  )
 }
--- src/components/CodeCell/CodeCell.tsx
+++ src/components/CodeCell/CodeCell.tsx
@@ -2,28 +2,32 @@ import React, {useState,useEffect} from 'react'
 import CodeEditor from '../CodeEditor/CodeEditor'
 import Preview from '../Preview/Preview';
 import bundle from '../../bundler/bundle';
+import {useActions} from '../../hooks/useActions'
 import Resizable from '../Resizable/Resizable';
-const CodeCell =()=> {
-
-  const [input, setInput] = useState('');
+import {Cell} from '../../redux/cell'
+interface CodeCellProps{
+  cell: Cell
+}
+const CodeCell:React.FC<CodeCellProps> =({cell})=> {
   const [code, setCode] = useState('');
   const [err, setErr] = useState('');
+  const {updateCell} = useActions();
   useEffect(() => {
     const timer = setTimeout(async()=>{
-      const output =await bundle(input);
+      const output =await bundle(cell.content);
       setCode(output.code);
       setErr(output.err)
     },1000)
     return () => {
       clearTimeout(timer);
     };
-  }, [input]);
+  }, [cell.content]);
 
   return (
     <Resizable direction='vertical'>
     <div style={{height:'100%',display:'flex'}}>
       <Resizable direction='horizontal'>
-      <CodeEditor initialValue="const a= 1;" onChange={(e)=>setInput(e)}/>
+      <CodeEditor initialValue={cell.content} onChange={(e)=>updateCell(cell.id,e)}/>
       </Resizable>
     <Preview code={code} error={err}/>
     </div>
